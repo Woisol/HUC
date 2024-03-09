38915ef557277661cb6bf3677ced02d73a688443
Woisol-G <Woisol-G@outlook.com>
2024-03-09 22:32:36 +0800

    整合

--- src/Components/BG_Gear.js
+++ src/Components/BG_Gear.js
@@ -2,7 +2,7 @@ import { Component } from "react";
 import setting from "../Asset/setting.svg";
 
 
-class Gear extends Component {
+class BGGear extends Component {
 	// constructor(props) {
 	// 	super(props);
 	// 	// this.state = { rotate: 0 }
@@ -17,18 +17,28 @@ class Gear extends Component {
 	// 	clearInterval(this.interval)
 	// }
 	// ！最佳实践的旋转并不是用state实现的…………
+
 	render() {
-		const { size, top, right, isRotate } = this.props;
+		const { isRotate } = this.props;
 		// ！！！！！Via TY，类组件必须这样才能读取props！下面三个都不行
 		return (
-			// ！两种方式都不对…………<img className="absolute" style={{ width: `${props.size}px`, height: `${props.size}px`, top: `${props.top}px`, right: `${props.right}px`, transition: "0.1s", transformt: `rotate(${this.state.rotate}}deg)` }} src={setting} alt="" />
-			// <img className="absolute" style={{ width: `${this.state.size}px`, height: `${this.state.size}px`, top: `${this.state.top}px`, right: `${this.state.right}px`, transition: "0.1s", transformt: `rotate(${this.state.rotate}}deg)` }} src={setting} alt="" />
-			// <img className="absolute" style={{ width: `${this.props.size}px`, height: `${this.props.size}px`, top: `${this.props.top}px`, right: `${this.props.right}px`, transition: "0.1s", transformt: `rotate(${this.state.rotate}}deg)` }} src={setting} alt="" />
-			<img className={`absolute ${isRotate ? 'rotate' : ''}`} style={{ width: `${size}px`, height: `${size}px`, top: `${top}px`, right: `${right}px`, transition: "0.1s" }
-			} src={setting} alt="" />
+			<div className="w-full h-fit bg-gray-400">
+				{/* // ！两种方式都不对…………<img className="absolute" style={{ width: `${props.size}px`, height: `${props.size}px`, top: `${props.top}px`, right: `${props.right}px`, transition: "0.1s", transformt: `rotate(${this.state.rotate}}deg)` }} src={setting} alt="" /> */}
+				{/* <img className="absolute" style={{ width: `${this.state.size}px`, height: `${this.state.size}px`, top: `${this.state.top}px`, right: `${this.state.right}px`, transition: "0.1s", transformt: `rotate(${this.state.rotate}}deg)` }} src={setting} alt="" /> */}
+				{/* <img className="absolute" style={{ width: `${this.props.size}px`, height: `${this.props.size}px`, top: `${this.props.top}px`, right: `${this.props.right}px`, transition: "0.1s", transformt: `rotate(${this.state.rotate}}deg)` }} src={setting} alt="" /> */}
+				{/* <Gear size="200px" top="30px" right="319px" /> */}
+				{/* <img className="absolute" style={{ width: "200px", height: "200px", top: "30px", right: "319px" }} src={setting} alt="" />
+				<img className="absolute" style={{ width: "250px", height: "250px", top: "102px", right: "123px" }} src={setting} alt="" />
+				<img className="absolute" style={{ width: "250px", height: "250px", top: "106px", right: "470px" }} src={setting} alt="" /> */}
+				{/* ！记得{} */}
+
+				<img className={`absolute ${isRotate ? 'rotate' : ''}`} style={{ width: "200px", height: "200px", top: "30px", right: "319px", transition: "0.1s" }} src={setting} alt="" />
+				<img className={`absolute ${isRotate ? 'rotate' : ''}`} style={{ width: "250px", height: "250px", top: "102px", right: "123px", transition: "0.1s" }} src={setting} alt="" />
+				<img className={`absolute ${isRotate ? 'rotate' : ''}`} style={{ width: "250px", height: "250px", top: "106px", right: "470px", transition: "0.1s" }} src={setting} alt="" />
+			</div>
 
 		)
 
 	}
 }
-export default Gear;
+export default BGGear;
