03d4345447eca8b8b6d56c7082352f5c3d31db90
Woisol-G <Woisol-G@outlook.com>
2024-03-10 19:22:09 +0800

    ！！！实现文件读取！！！ 艹没想象的那么简单呜呜呜 react本身不能读取，涉及到进程通讯 又涉及到react导入electorn不同的方式………… 还涉及到主进程主动发送信息…………并不是用ipcMain………………

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const createWindow = () => {
 	const win = new BrowserWindow({
 		width: 1440,
@@ -7,12 +7,23 @@ const createWindow = () => {
 		webPreferences: {
 			nodeIntegration: true,
 			contextIsolation: false,
+			// preload: "./preload.js"
 		},
 	});
 	// win.removeMenu();
 	// win.loadFile("./src/test/ScollTest.html")
 	win.loadURL('http://localhost:3000/');
+	win.webContents.openDevTools();
+	// app.whenReady().then(createWindow());
+	// ！艹为什么一定是这个写法…………上面那个之前明明行的…………
+	const runtimeLogFileStream = require("fs")
+	ipcMain.on("GetRuntimeLog", DeliverContent)
+	function DeliverContent() {
+		// ipcMain.("ContentUpdate", runtimeLogFileStream.readFileSync("./src/Monitor/runtimeLog.rlf", "utf8"));
+		win.webContents.send("ContentUpdate", runtimeLogFileStream.readFileSync("./src/Monitor/runtimeLog.rlf", "utf8"))
+		// ！ipcMain不能主动发送消息…………有点复杂…………
+	}
+	runtimeLogFileStream.watch("./src/Monitor/runtimeLog.rlf", DeliverContent)
 }
-// app.whenReady().then(createWindow());
 app.on('ready', createWindow);
-// ！艹为什么一定是这个写法…………上面那个之前明明行的…………
+//!艹注意这个不要放里面…………
--- src/Components/Dashboard/Console.js
+++ src/Components/Dashboard/Console.js
@@ -1,8 +1,22 @@
 import { useState } from "react";
+// import { ipcRenderer } from 'electron';
+const { ipcRenderer } = window.require("electron");
+// ！艹又忘记了要先有preload.js………………
+// ！！！！！！！！！大难题！！！
+// ！注意在react里面要调用Electron必须要用window.require！
+// ！原因见下面两篇博客
+//https://blog.csdn.net/weixin_42728767/article/details/129499927
+// https://segmentfault.com/a/1190000021898410
+//！搜的electron react fs才出来这么多精品文章！！！
 
 export default function Console(props) {
-	const content = "2024-03-07 20:56 STAT APP Started";
-	// const
+	// const runtimeLogFileStream = require("fs").createReadStream("../../Monitor/runtimeLog.rlf");
+	//！ wok注意渲染进程没有权限！！！
+	const [content, UpdateContent] = useState("2024-03-07 20:56 STAT APP Started");
+	ipcRenderer.send("GetRuntimeLog");
+	ipcRenderer.on("ContentUpdate", function (event, arg) {
+		UpdateContent(arg);
+	})
 	const [isOpen, setIsOpen] = useState(false);
 	return (
 		<div className="absolute right-0 rounded-2xl z-10 bg-gradient-to-t from-black to-gray-500 text-wrap text-xs text-white" style={isOpen ? { width: "300px", height: "600px", top: "50%", padding: "20px", transform: "translateY(-50%)", transition: "0.5s" } : { width: "20px", height: "120px", top: "50%", transform: "translateY(-50%)", transition: "0.5s" }} onClick={toggleIsOpen}>{isOpen ? content : ""}</div>
