356332ce3d740a913c5750d9267d4f8edc86a883
Woisol-G <Woisol-G@outlook.com>
2024-03-26 16:30:52 +0800

    feat(SideBar): 使用js几乎完美实现位置检测！！！
    尝试使用CSS的animation但是似乎electron不支持
    使用js监测scollTop结果对象不对一直不行
    后来还是用了新学的useRef实现的/赞

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,41 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import "./MainView.css";
 import PageDashBoard from './Page_DashBoard.tsx';
 import PageAppDetail from './Page_AppRunTime.tsx';
 import SlideBar from './Components/Layout/SideBar.tsx';
+import { Tab } from '@headlessui/react';
 const ipcRenderer = window.require('electron').ipcRenderer;
 const useState = React.useState;
 
 // ！一定不要再设置在function里面了………………反复调用又22出错…………
 export default function App() {
   const [isMonitorRunning, setIsMonitorRunning] = useState(true);
+  //**----------------------------ScollBarPosition-----------------------------------------------------
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const ScollRef = useRef(null);
+  // window.addEventListener("scroll", () => {
+  //   setSelectedIndex(document.documentElement.scrollTop / window.innerHeight);
+  // })
+
   ipcRenderer.on('MonitorStateChange', (event, arg) => {
     setIsMonitorRunning(arg);
   });
+  // document.getElementById("View").addEventListener('scroll', (event) => {
+
+  // })
 
   return (
-    <>
+    <Tab.Group selectedIndex={selectedIndex}>
+      {/* //！！用了as div才能使用view！ */}
       <SlideBar />
-      <div className='absolute w-screen h-screen overflow-auto overflow-x-hidden scroll-smooth snap-y snap-mandatory -z-50 hideScollBar'>
+      <Tab.Panels ref={ScollRef} as='div' id='View' onScroll={(e) => handleScoll(e)} className='absolute w-screen h-screen overflow-auto overflow-x-hidden scroll-smooth snap-y snap-mandatory -z-50 hideScollBar'>
         {/* ！wok！！！！才知道这个overflow-auto是必须的！！！！！！！ */}
         {/* ！同时这里加absolute可以防止子元素相对root定位而超过滚动条……………… */}
         {/* ！！！！必须用absolute！！用relative可能导致鼠标在子元素上时无法滚动 */}
         <PageDashBoard isMonitorRunning={isMonitorRunning} toggleState={toggleState} />
         <PageAppDetail />
-      </div>
-    </>
+      </Tab.Panels>
+    </Tab.Group>
   );
   function toggleState() {
     setIsMonitorRunning(!isMonitorRunning);
@@ -35,4 +47,10 @@ export default function App() {
     }
   }
   // console.log(isMonitorRunning);
+  function handleScoll({ e }) {
+    // td性能优化
+    //e.target document.documentElement.scrollTop || document.body.scrollTop;
+    // ！！！终于实现！！注意scollTop必须获取正确对象才能得到！！用上面那几个都不行！！！！！！！
+    setSelectedIndex((ScollRef.current.scrollTop + window.innerHeight / 2) / window.innerHeight)
+  }
 }
--- src/Components/Layout/SideBar.tsx
+++ src/Components/Layout/SideBar.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
+import { useState } from 'react';
 import SlideBarIcon1 from "../../Asset/dashboard.svg";
 import SlideBarIcon2 from "../../Asset/AppDetail.svg";
 import SlideBarIcon3 from "../../Asset/setting_SlideBar.svg";
+import { Tab } from '@headlessui/react';
+import { event } from 'jquery';
 // import $ from "jquery";
 
 // function pageScoll(props) {
@@ -16,16 +19,26 @@ const SlideBarOpProps = [
 function SlideBarOption(props, index) {
 	// ！是的可以这样多搞几个参数的，解决没有key的问题
 	return (
-		<a key={index} href={"#" + props.PageID} className="flex items-center w-full overflow-hidden transition-all rounded-2xl hover:bg-blue-300 hover:text-xl"> <img className="m-1 w-14 h-14" src={props.icon} alt={props.title} />{props.title}</a>
-		// ！额这里img必须w和h都设定了才能真正设定………………
+		<Tab>
+			{({ selected }) =>
+				<a key={index} href={"#" + props.PageID} className={`flex items-center w-full overflow-hidden transition-all rounded-2xl hover:text-xl ${selected ? 'bg-blue-300 border-l-4 rounded-l-none  border-blue-500' : 'hover:bg-blue-300'} `}>
+					<img className="m-1 w-14 h-14" src={props.icon} alt={props.title} />
+					{props.title}
+				</a>
+			}
+			{/* // ！额这里img必须w和h都设定了才能真正设定……………… */}
+		</Tab>
 	)
 }
 function SlideBar(props) {
+
 	return (
 		// <div className="fixed left-0 flex flex-col justify-center w-0 w-16 h-0 py-2 transition-all -translate-y-1/2 bg-blue-200 shadow-2xl sm:h-fit rounded-2xl items-cente hover:w-48 hover:px-3 top-1/2">
-		<div className="fixed left-0 flex flex-col justify-center w-0 py-1 transition-all -translate-y-1/2 bg-blue-200 rounded-md shadow-2xl h-fit sm:w-16 sm:h-fit sm:rounded-2xl items-cente hover:w-20 sm:hover:w-48 hover:px-3 top-1/2">
+		// <>
+		<Tab.List className="fixed left-0 flex flex-col hover:pl-2 justify-center w-0 py-1 transition-all -translate-y-1/2 bg-blue-200 rounded-md shadow-2xl h-fit sm:w-16 sm:h-fit sm:rounded-2xl items-cente hover:w-[72px] sm:hover:w-48 top-1/2 group:">
 			{SlideBarOpProps.map((item, index) => { return SlideBarOption(item, index) })}
-		</div>
+		</Tab.List>
+		// </>
 	);
 }
 export default SlideBar;
